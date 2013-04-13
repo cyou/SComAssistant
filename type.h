a8967c6a91311bba84c20a78614f27bf7a57d3d6
fjling <jialingfu@hotmail.com>
2013-04-13 22:08:16 +0800

    file: add a type.h
    Used for type define
    
    Signed-off-by: fjling <jialingfu@hotmail.com>

---
+++ type.h
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+
+#ifndef TYPEDEFINE_H
+#define TYPEDEFINE_H
+
+typedef unsigned char  uint8;                   /* defined for unsigned 8-bits integer variable 	无符号8位整型变量  */
+typedef signed   char  int8;                    /* defined for signed 8-bits integer variable		有符号8位整型变量  */
+typedef unsigned short uint16;                  /* defined for unsigned 16-bits integer variable 	无符号16位整型变量 */
+typedef signed   short int16;                   /* defined for signed 16-bits integer variable 		有符号16位整型变量 */
+typedef unsigned int   uint32;                  /* defined for unsigned 32-bits integer variable 	无符号32位整型变量 */
+typedef signed   int   int32;                   /* defined for signed 32-bits integer variable 		有符号32位整型变量 */
+typedef float          fp32;                    /* single precision floating point variable (32bits) 单精度浮点数（32位长度） */
+typedef double         fp64;                    /* double precision floating point variable (64bits) 双精度浮点数（64位长度） */
+
+#endif
